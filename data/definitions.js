b90bef36419b37eb23d661c6dfb6cc3089125c15
Caleb Williams <caleb.d.williams@gmail.com>
2016-04-15 09:17:56 -0500

    Create definitions.js

---
+++ data/definitions.js
@@ -0,0 +1,22 @@
+module.exports = [
+	{
+		term: 'Do',
+		defined: 'A deer, a female deer'
+	}, 
+	{
+		term: 'Re',
+		defined: 'A drop of golden sun'
+	}, 
+	{
+		term: 'Me',
+		defined: 'A name I call myself'
+	}, 
+	{
+		term: 'Fa',
+		defined: 'A long, long way to run'
+	},
+	{
+		term: 'So',
+		defined: 'A needle pulling thread'
+	}
+];
